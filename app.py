561392e3192b90883f93d27aca13307ac9c74ad5
Mekhi Rhodie <mekhirhodie@gmail.com>
2020-09-27 17:49:45 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from random import randint
+
 game_active = True
 user_wins = 0
 user_score = 0
@@ -8,34 +10,46 @@ cpu_choices = ['Rock', 'Paper', 'Scissors']
 
 def game(user_score, cpu_score):
     user_choice = input("Rock, Paper, or Scissors?:  ").title()
-    cpu_choice = set(cpu_choices).pop()
+    cpu_choice = cpu_choices[randint(0, 2)]
+    print(f"You chose:  {user_choice}")
+    print(f"The computer chose:  {cpu_choice}")
 
-    if user_choice == "Rock" and cpu_choice == 'Paper':
+    if user_choice == 'Rock' and cpu_choice == 'Paper':
         cpu_score += 1
-        print(f"CPU Wins! :({cpu_score}")
-    elif (user_choice == "Rock" and cpu_choice == 'Scissors'):
+        print(f"CPU Wins! :(  {cpu_score}")
+        return
+    elif user_choice == 'Rock' and cpu_choice == 'Scissors':
+        user_score += 1
+        print(f"User Wins!  {user_score}")
+        return
+    elif user_choice == 'Paper' and cpu_choice == 'Rock':
+        user_score += 1
+        print(f"User Wins!  {user_score}")
+        return
+    elif user_choice == 'Paper' and cpu_choice == 'Scissors':
+        cpu_score += 1
+        print(f"CPU Wins! :(  {cpu_score}")
+        return
+    elif user_score == 'Scissors' and cpu_choice == 'Paper':
         user_score += 1
         print(f"User Wins! {user_score}")
-    else:
-        print("This round is a tie!")
-
-    if user_choice == "Paper" and cpu_choice == "Scissors":
+        return
+    elif user_choice == 'Scissors' and cpu_choice == 'Rock':
         cpu_score += 1
-        print(f"CPU Wins! :({cpu_score}")
-    elif user_score == "Scissors" and cpu_choice == "Paper":
-        user_score += 1
-        print(f"User Wins! {user_score}")
+        print(f"CPU Wins! :(  {cpu_score}")
+        return
     else:
         print("This round is a tie!")
+        return
 
+
+while game_active == True:
+    game(user_score, cpu_score)
     if user_score == 3:
-        print("User Has Won the Game!!")
-        user_wins += 1
+        print("You have Won the Game!")
         game_active = False
+        break
     elif cpu_score == 3:
-        print("CPU Has Won the Game! X(")
-        cpu_wins += 1
+        print("The CPU has Won the Game! X(")
         game_active = False
-
-
-game(user_score, cpu_score)
+        break
